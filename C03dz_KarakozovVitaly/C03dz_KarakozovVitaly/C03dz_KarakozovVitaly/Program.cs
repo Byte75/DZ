af033d4a82afa3345c79f5e2681cb31b75f56b0e
Vitaly <Byte75@yandex.ru>
2020-08-03 15:51:13 +0400

    1.	30 раз была вызвана операция склеивания строк. 2.	Максимальное кол-во оперативной памяти было выделено под приложение в процессе работы - 2 Гб. 3.	Ни разу не была была инициирована сборка мусора.
    Скрины профайлера прилагаю.

---
+++ C03dz_KarakozovVitaly/C03dz_KarakozovVitaly/C03dz_KarakozovVitaly/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C03dz_KarakozovVitaly
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "A";
+
+            while (true) 
+            {
+                str += str;
+                Console.WriteLine(str.Length);
+            }
+
+
+        }
+    }
+}
